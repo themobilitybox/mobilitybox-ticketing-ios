8153e9b185d4bc2245a57c9e3416a09c4ac1a904
tim.krusch <tim@vesputi.com>
2022-06-02 22:41:29 +0200

    add templates

--- Package.swift
+++ Package.swift
@@ -20,9 +20,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "MobilityboxSDK",
-            dependencies: []),
-        .testTarget(
-            name: "MobilityboxSDKTests",
-            dependencies: ["MobilityboxSDK"]),
+            dependencies: [],
+            resources: [
+                .copy("Templates")
+            ]
+        )
     ]
 )
