94b22baa81f77fe926ba5917abb735239b0daa04
tim.krusch <tim@vesputi.com>
2022-07-07 10:47:44 +0200

    update ticket attributes to be vars instead of let
    remove description

--- Sources/Mobilitybox/Models/MobilityboxTicket.swift
+++ Sources/Mobilitybox/Models/MobilityboxTicket.swift
@@ -13,23 +13,15 @@ public class MobilityboxTicket: Identifiable, Codable, Equatable {
     }
     
     public let id: String
-    public let product: MobilityboxOrderedProduct
-    public let ticket: MobilityboxTicketDetails
-    public let area: MobilityboxTicketArea
-    public let valid_from: String
-    public let valid_until: String
-    public let ticket_created_at: String
-    
+    public var product: MobilityboxOrderedProduct
+    public var ticket: MobilityboxTicketDetails
+    public var area: MobilityboxTicketArea
+    public var valid_from: String
+    public var valid_until: String
+    public var ticket_created_at: String
     
     func getTitle() -> String {
         return "\(area.properties.city_name) - \(product.getTitle())"
-    }
-    
-    func getDescription() -> String {
-        let valid_from_formatted = MobilityboxFormatter.shortDateAndTime.string(from: MobilityboxFormatter.isoDateTime.date(from: valid_from)!)
-        let valid_until_formatted = MobilityboxFormatter.shortDateAndTime.string(from: MobilityboxFormatter.isoDateTime.date(from: valid_until)!)
-        
-        return "valid from:\t\(valid_from_formatted)\nvalid until:\t\(valid_until_formatted)\nin Zone:\t\(area.properties.local_zone_name)"
     }
     
     public func isValid() -> Bool {
